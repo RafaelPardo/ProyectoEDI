4d0ce7e7cae1a8c3cd035dd71c13ca7c692df59a
RafaelPardo <rafaelpardo95@gmail.com>
2022-12-05 16:49:05 -0300

    Agregamos empleados y servicios de empleados y cargamos algunos datos

--- src/employees/employees.controller.ts
+++ src/employees/employees.controller.ts
@@ -1,4 +1,10 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+import { EmployeesService } from './employees.service';
 
 @Controller('employees')
-export class EmployeesController {}
+export class EmployeesController {
+    constructor(private readonly employeesService: EmployeesService) {}
+    @Get()
+    getEmployees() {
+        return this.employeesService.getEmployees();
+    }}
--- src/employees/employees.service.ts
+++ src/employees/employees.service.ts
@@ -1,4 +1,19 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class EmployeesService {}
+export class EmployeesService {
+    getEmployees() {
+        return [{
+        id: 1,
+        name: 'Rafael'
+    },
+    {
+        id: 2,
+        name: 'Juan'
+    },
+    {
+        id: 3,
+        name: 'Miguel'
+    }]
+    }
+}
